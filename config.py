c8657f59b7887853cc7fefe92fd797ed9e13ac75
me10die <gardosemelodie@gmail.com>
2023-06-04 14:04:48 +0800

    creating database connection

---
+++ config.py
@@ -0,0 +1,9 @@
+from app import app
+from flaskext.mysql import MYSQL
+
+mysql = MySQL()
+app.config['MYSQL_DATABASE_USER'] = 'root'
+app.config['MYSQL_DATABASE_PASSWORD'] = ''
+app.config['MYSQL_DATABASE_DB'] = 'rest-api'
+app.config['MYSQL_DATABASE_HOST'] = 'localhost'
+mysql.init_app(app)
